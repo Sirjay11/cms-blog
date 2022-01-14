b4439b594a6c03ea5b4c4e79a534bb9e9704ef39
James-Leroy <jlanvja@gmail.com>
2022-01-13 22:49:10 -0500

    additions

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,7 @@
 import React from "react";
-import {useRouter} from "next/router";
-import { AdjacentPosts } from "../../sections";
-import { getPosts, getPostDetails } from "../../services";
+import { useRouter } from "next/router";
+import { GetStaticPaths } from "next";
+
 import {
   PostDetail,
   Categories,
@@ -11,13 +11,16 @@ import {
   CommentsForm,
   Loader,
 } from "../../components";
+import { getPosts, getPostDetails } from "../../services";
+import { AdjacentPosts } from "../../sections";
 
 const PostDetails = ({ post }) => {
   const router = useRouter();
 
-  if(router.isFallback){
-    return <Loader />
+  if (router.isFallback) {
+    return <Loader />;
   }
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -41,19 +44,22 @@ const PostDetails = ({ post }) => {
     </div>
   );
 };
-
 export default PostDetails;
 
+// Fetch data at build time
 export async function getStaticProps({ params }) {
   const data = await getPostDetails(params.slug);
   return {
-    props: { post: data },
+    props: {
+      post: data,
+    },
   };
 }
 
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
   const posts = await getPosts();
-
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
     fallback: true,
